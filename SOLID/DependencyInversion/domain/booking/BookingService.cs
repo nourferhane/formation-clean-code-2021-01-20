e1599b8e2d57d420e7e16d2951cd0da84ddbc9fb
Cédric Rup <cedric.rup@gmail.com>
2020-09-28 23:26:23 +0200

    SOLID

---
+++ SOLID/DependencyInversion/domain/booking/BookingService.cs
@@ -0,0 +1,20 @@
+using SOLID.DependencyInversion.infrastructure;
+
+namespace SOLID.DependencyInversion.domain.booking
+{
+    public class BookingService
+    {
+        private readonly IAvailabilityDao bookings;
+
+        public BookingService(IAvailabilityDao bookings)
+        {
+            this.bookings = bookings;
+        }
+
+        public BookingOutcome Book()
+        {
+            bool successful = bookings.IsAvailable();
+            return new BookingOutcome(successful);
+        }
+    }
+}
