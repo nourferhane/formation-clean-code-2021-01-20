1fb7911f45e88c62ffeac57418ace0d9c60f329b
Cédric Rup <cedric.rup@gmail.com>
2021-01-21 13:17:36 +0100

    Oops, fix a test !

--- SOLID/DependencyInversion/infrastructure/AvailabilityDaoImpl.cs
+++ SOLID/DependencyInversion/infrastructure/AvailabilityDaoImpl.cs
@@ -2,7 +2,7 @@ namespace SOLID.DependencyInversion.infrastructure
 {
     public class AvailabilityDaoImpl : IAvailabilityDao {
     
-        public bool IsAvailable() {
+        public virtual bool IsAvailable() {
             //En realite il y aurait une dependance vers une base de donnéesS...
             return true; 
         }
